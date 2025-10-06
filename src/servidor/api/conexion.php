55dee4ee9270897eb4691f692a6d3114b6e3bb13
ferran <ferransansaloni@gmail.com>
2025-10-06 23:53:43 +0200

    Casi todo el codigo comentado
    Falta únicamente comentar el codigo de Arduino

--- src/servidor/api/conexion.php
+++ src/servidor/api/conexion.php
@@ -1,13 +1,34 @@
 <?php
+// ------------------------------------------------------------------
+// Fichero: conexion.php
+// Autor: Ferran Sansaloni Prats
+// Fecha: 01/10/2025
+// ------------------------------------------------------------------
+// Descripción:
+//   Establece la conexión a la base de datos MySQL y configura los
+//   encabezados para respuestas JSON
+// ------------------------------------------------------------------
+
+// Configuración de cabeceras HTTP
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
+// Datos para la conexión
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "mediciones_sprint0";
 
+// Conexión a la base de datos
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Comprobar errores de conexión
 if ($conn->connect_error) {
     die(json_encode(["error" => "Conexión fallida: " . $conn->connect_error]));
 }
+
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
+?>
